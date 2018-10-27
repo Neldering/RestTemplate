f0d7b9e647b5252958f70a379ede571284f321d2
neldering <nick.eldering@gmail.com>
2018-10-27 11:16:44 -0400

    Initial Commit
    Rest Template

---
+++ src/main/java/com/playingaround/test/Controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.playingaround.test.Controller;
+
+import com.playingaround.test.Model.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format("Hello %s", name));
+    }
+}
